cead569081fb1426e2a44e4806218e5346922810
LutherLeonard <93784028+LutherLeonard@users.noreply.github.com>
2023-05-03 11:27:44 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlPanelActrivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String output1 {
+            set { textBox1.Text = value; }
+        }
+        public String output2 {
+            set { textBox2.Text = value; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
